6eaacff26003ec9312058d5439186fbb5667cdb1
Vasileios Goysetis <83865356+Erevos-IV@users.noreply.github.com>
2021-05-30 12:51:00 +0300

    Create Payments.sql

---
+++ Database/Tables/Payments.sql
@@ -0,0 +1,9 @@
+CREATE TABLE Payments(
+	PaymentID INT NOT NULL AUTO_INCREMENT,
+    CustomerID INT NOT NULL,
+    PaymentDate DATE NOT NULL,
+    TotalAmount DOUBLE NOT NULL,
+    
+    PRIMARY KEY(PaymentID),
+    FOREIGN KEY(CustomerID) REFERENCES Customers(CustomerID)
+)ENGINE = InnoDB;
