fd7185dfe1884a8b8340c81fe9b24b152006f96f
Vasileios Goysetis <83865356+Erevos-IV@users.noreply.github.com>
2021-05-30 17:14:26 +0300

    Create EstablisingConnection.py

---
+++ Connection/EstablisingConnection.py
@@ -0,0 +1,7 @@
+
+# imports.
+import mysql.connector
+
+# Connector.
+mydb = mysql.connector.connect(host="localhost", user="vasilhs", password="bill", database ="BloodDonors")
+mycursor = mydb.cursor()
