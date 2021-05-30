5dee7a4d27744f7bbe6e053a5b6b1f59e70841f2
Vasileios Goysetis <83865356+Erevos-IV@users.noreply.github.com>
2021-05-30 12:53:48 +0300

    Create OrderDetails.sql

---
+++ Database/Tables/OrderDetails.sql
@@ -0,0 +1,13 @@
+CREATE TABLE OrderDetails(
+	OrdetID INT NOT NULL AUTO_INCREMENT,
+    OrderID INT NOT NULL,
+    ProductCode VARCHAR(5) NOT NULL,
+    Quantity INT NOT NULL,
+    Discount FLOAT,
+    TotalPrice DOUBLE NOT NULL,
+    
+    PRIMARY KEY(OrdetID),
+    FOREIGN KEY(OrderID) REFERENCES Orders(OrderID),
+    FOREIGN KEY(ProductCode) REFERENCES Products(ProductCode)
+)ENGINE = InnoDB;
+
