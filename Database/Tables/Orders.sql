bd8540af56dd3deead849cb94f2dd9bd0b22d002
Vasileios Goysetis <83865356+Erevos-IV@users.noreply.github.com>
2021-05-30 12:54:22 +0300

    Create Orders.sql

---
+++ Database/Tables/Orders.sql
@@ -0,0 +1,13 @@
+CREATE TABLE Orders(
+	OrderID INT NOT NULL AUTO_INCREMENT,
+    CustomerID INT NOT NULL,
+    OrderDate DATE NOT NULL,
+    -- An order may not be shipped yet.
+    -- OrderDate and ShipDate may be the same if the customer gets his order in the store.
+    ShipDate DATE,
+    Comments TEXT,
+    
+    PRIMARY KEY(OrderID),
+    FOREIGN KEY(CustomerID) REFERENCES Customers(CustomerID)
+    
+)AUTO_INCREMENT = 1 ENGINE = InnoDB;
