d4b98c56b64c0c46b130bcb7d972c71381858327
Vasileios Goysetis <83865356+Erevos-IV@users.noreply.github.com>
2021-05-31 08:44:27 +0300

    Delete Connection directory

--- Connection/EstablisingConnection.py
+++
@@ -1,7 +0,0 @@
-
-# imports.
-import mysql.connector
-
-# Connector.
-mydb = mysql.connector.connect(host="localhost", user="vasilhs", password="bill", database ="BloodDonors")
-mycursor = mydb.cursor()
