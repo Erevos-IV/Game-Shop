4883545908f3466efaf3d60f50b44fb299e9e90d
Vasileios Goysetis <83865356+Erevos-IV@users.noreply.github.com>
2021-05-31 08:48:46 +0300

    Create All_indexes.sql

---
+++ Database/Indexes/All_indexes.sql
@@ -0,0 +1,11 @@
+CREATE INDEX EmpIDX
+ON employees(EmployeeID);
+
+CREATE INDEX CustomerIDX
+ON customers(CustomerID);
+
+CREATE INDEX StoresIDX
+ON stores(StoreID);
+
+CREATE INDEX OrdersIDX
+ON orders(OrderID);
